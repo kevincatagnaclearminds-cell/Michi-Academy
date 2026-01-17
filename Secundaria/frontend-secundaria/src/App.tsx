7442e3a3902ec620b4818070300b7372bf504722
Edwin <142370332+Edwin-Alomoto@users.noreply.github.com>
2026-01-17 16:35:26 -0500

    chore: add @vitejs/plugin-react to devDependencies in package.json

---
+++ Secundaria/frontend-secundaria/src/App.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+
+// Importar uno de los mÃ³dulos de Secundaria como ejemplo
+// Puedes cambiar esto por el mÃ³dulo que desees mostrar
+import { UserVideosPage } from './modules/games/secundaria/videos/pages/UserVideosPage';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <UserVideosPage />
+    </div>
+  );
+};
+
+export default App;
