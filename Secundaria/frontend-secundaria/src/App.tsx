03f9ebf089233e4a51ac7a90ff5bac357e6fa718
Edwin <142370332+Edwin-Alomoto@users.noreply.github.com>
2026-01-21 21:39:02 -0500

    change

---
+++ Secundaria/frontend-secundaria/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <h1>Michi Academy - Secundaria</h1>
+      <p>Bienvenido al frontend de secundaria</p>
+    </div>
+  );
+};
+
+export default App;
